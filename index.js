7739065b743ec78ac136257dd8658242300306d0
邢硕 <xingshuo@xdf.cn>
2016-12-12 16:20:03 +0800

    修改

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
 var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
+var chalk = require('chalk');
 
 // 常量
-const PLUGIN_NAME = 'detectionDomain';
-
-function domainStream(domain) {
-    var stream = through();
-    stream.write(domain);
-    return stream;
-}
+const PLUGIN_NAME = 'checkDomain';
 
 // 插件级别函数 (处理文件)
-function detectionDomain(domain) {
+function checkDomain(domain) {
     if (!domain) {
         throw new PluginError(PLUGIN_NAME, 'Missing domain text!');
     }
-
-    domain = new Buffer(domain); // 预先分配
-
     // 创建一个让每个文件通过的 stream 通道
     var stream = through.obj(function(file, enc, cb) {
         if (file.isBuffer()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+            var str = String(file.contents);
+            domain.map(function(item){
+                if(str.indexOf(item) > 0){
+                    gutil.log(chalk.red('包含写死域名'), item);
+                }
+            });
             return cb();
         }
-
         if (file.isStream()) {
-            // 定义转换内容的 streamer
-            var streamer = domainStream(prefixText);
-            // 从 streamer 中捕获错误，并发出一个 gulp的错误
-            streamer.on('error', this.emit.bind(this, 'error'));
-            // 开始转换
-            file.contents = file.contents.pipe(streamer);
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+            return cb();
         }
-
         // 确保文件进去下一个插件
         this.push(file);
         // 告诉 stream 转换工作完成
         cb();
     });
-
     // 返回文件 stream
     return stream;
 }
 
 // 暴露（export）插件的主函数
-module.exports = detectionDomain;
+module.exports = checkDomain;
--- test/index.js
+++ test/index.js
@@ -1,20 +1,10 @@
 var assert = require('assert');
-var addFn = require('../index');
+var checkDomain = require('../index');
 
-describe('addFn', function () {
+describe('checkDomain', function () {
     describe('usage', function () {
         it('require(this-module) returns a function', function () {
-            assert(typeof addFn === 'function', 'addFn should be a function')
-        });
-        it('addFn(number) returns a function', function () {
-            var add30 = addFn(30);
-            assert(typeof add30 === 'function',
-                'addFn(number) should be a function')
-        });
-
-        it('addFn(x)(y) === x + y', function () {
-            assert(addFn(30)(50) === 80,
-                'addFn(x)(y) should strictly equal to x + y')
+            assert(typeof checkDomain === 'function', 'checkDomain should be a function')
         });
     })
 });
